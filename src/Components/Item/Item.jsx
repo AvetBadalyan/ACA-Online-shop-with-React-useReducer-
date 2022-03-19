f55dfcece13d4b157b7f52052009006d31c03194
AvetBadalyan <avetbadalyan@gmail.com>
2022-03-19 12:27:29 +0400

    feat: styling item and adding counter with buttons

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import "./Item.css"
-import Products from '../Products/Products';
+
+function Counter({count=0, dispatch, id}) {
+    return (
+      <div className='Counter'>
+        <button>-</button>
+        <div>{count}</div>
+        <button>+</button>
+      </div>
+    );
+}
 
 export default function Item( {id, price, title, image }) {
   return (
     <div className="Wrapper">
       <img src={image} alt="image" width="250px" height="300px" />
       <div className="title">{title.slice(0, 12)}...</div>
-      <div className="price">$ {price.toFixed(2)}</div>
+          <div className="price">$ {price.toFixed(2)}</div>
+          <Counter/> 
       <p>Total Count: </p>
-      <div>
-        <button >-</button>
-        <button >+</button>
-      </div>
     </div>
   );
 }
